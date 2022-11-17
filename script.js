1a96c7f0304cbaf6d095d1f993ffa22f95f15b2a
Rince Mathew <rincemathewmanattu@gmail.com>
2022-11-17 14:52:31 +0530

    rules and close event handlers

---
+++ script.js
@@ -0,0 +1,10 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
