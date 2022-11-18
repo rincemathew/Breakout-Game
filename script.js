72d4463332626f5eb5c510d07497d0c4902ef84f
Rince Mathew <rincemathewmanattu@gmail.com>
2022-11-18 21:35:56 +0530

    draw ball

--- script.js
+++ script.js
@@ -5,6 +5,28 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 
+// Create ball props
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+  };
+  
+// Draw ball on canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+  }
+
+
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
 closeBtn.addEventListener('click', () => rules.classList.remove('show'));
+
+
